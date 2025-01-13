5749d2cc12fc18b1f173447b303c06866b2ac821
igor-ifrs <cigor80@gmail.com>
2025-01-12 23:39:02 -0300

    javascript features completed :hammer:

--- src/main.js
+++ src/main.js
@@ -1,24 +1,61 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const icon = document.querySelector(".icon-theme");
+const root = document.documentElement;
+const [so, page, local] = [...document.querySelectorAll(".info span")];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const THEMES = {
+    DARK: "dark",
+    LIGHT: "light",
+    NOT_DEFINED: "Não definido",
+    NO_PREFERENCE: "no-preference",
+};
 
-setupCounter(document.querySelector('#counter'))
+function localStorageTheme() {
+    try {
+        return localStorage.getItem("theme") || THEMES.NOT_DEFINED;
+    } catch (error) {
+        console.error("Erro ao acessar localStorage:", error);
+        return THEMES.NOT_DEFINED;
+    }
+}
+
+function SOTheme() {
+    const prefTheme = [THEMES.NO_PREFERENCE, THEMES.DARK, THEMES.LIGHT];
+    const theme = prefTheme.find((theme) => window.matchMedia(`(prefers-color-scheme: ${theme})`).matches) || THEMES.NOT_DEFINED;
+    return theme;
+}
+
+function loadTheme() {
+    const local = localStorageTheme();
+    if (local !== THEMES.NOT_DEFINED) {
+        setTheme(local);
+        return;
+    }
+    const sotheme = SOTheme();
+    if (sotheme !== THEMES.NO_PREFERENCE && sotheme !== THEMES.NOT_DEFINED) {
+        setTheme(sotheme);
+    }
+}
+
+function setTheme(theme) {
+    root.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+    local.textContent = localStorageTheme().toUpperCase();
+}
+
+function toggleTheme() {
+    const theme = root.getAttribute("data-theme") === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK;
+    setTheme(theme);
+}
+
+function updateInfo() {
+    so.textContent = SOTheme().toUpperCase();
+    page.textContent = root.getAttribute("data-theme").toUpperCase();
+    local.textContent = localStorageTheme().toUpperCase();
+}
+
+icon.addEventListener("click", toggleTheme);
+
+document.addEventListener("DOMContentLoaded", () => {
+    updateInfo();
+    loadTheme();
+});
